fc6f42647e56a4cabde35c24e0473ffc998f1d7d
IsacSnecker <isac.snecker@hotmail.com>
2024-02-20 15:07:49 +0100

    Added healthbar + bugfixes.

---
+++ Assets/BossHealthBarController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Damagable))]
+public class BossHealthBarController : MonoBehaviour
+{
+
+    GameObject boss;
+
+    public Damagable damagable;
+    public Image healthBar;
+    float currentHealth;
+    float maxHealth;
+    float healthPercentage;
+
+
+    
+
+    void Start() {
+        boss = GameObject.FindWithTag("Boss");
+        damagable = boss.GetComponent<Damagable>();
+        maxHealth = damagable.MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealth = damagable.Health;
+        healthPercentage = currentHealth / maxHealth;
+
+        healthBar.fillAmount = healthPercentage;
+
+    }
+}
